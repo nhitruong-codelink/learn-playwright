c152db83e1500a6168c147a0cf0fc00c6dcc71e1
Nhi Truong <nhi.truong@codelink.io>
2026-09-11 13:03:35 +0700

    fix failing test

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -3,7 +3,7 @@ import { createCustomer } from '../../src/data/customer';
 
 
 test.describe('Login page', () => {
-  test('user can login successfully', { tag: '@regression' },
+  test('user can login successfully', { tag: '@smoke' },
     async ({ pages, userAPI }) => {
       const customer = createCustomer();
 
